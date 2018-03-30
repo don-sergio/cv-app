103011399945beb8a4cac397f761d1ae4723251a
unknown <saad-fatihi>
2018-03-31 00:19:54 +0200

    update the banner with the full name.

--- src/CvBanner/CvBanner.jsx
+++ src/CvBanner/CvBanner.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import './CvBanner.css';
+import Typography from 'material-ui/Typography';
+import PropTypes from 'prop-types';
+import resumeJSON from '../models/curriculum-vitae.json'
+import { withTheme } from 'material-ui/styles'
+
+
 
 const CvBanner = (props) => {
+
+    const { theme } = props;
+    const style = {
+        color: theme.palette.primary.contrastText
+    }
+
     return (
         <div className="bnr-root bnr-banner-dimention">
+            <div className="bnr-degraded">
+                <div className="bnr-title">
+                <Typography style={style} align="center" variant="headline">{resumeJSON.personalDetails.fullName}</Typography>
+                <Typography style={style} align="center" variant="subheading">{resumeJSON.personalDetails.title}</Typography>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default CvBanner
+CvBanner.propTypes = {
+    theme: PropTypes.object.isRequired,
+};
+
+export default withTheme()(CvBanner);
 
--- src/CvCard/CvCard.jsx
+++ src/CvCard/CvCard.jsx
@@ -1,26 +1,15 @@
 import React from 'react'
 import './CvCard.css'
 import resumeJSON from '../models/curriculum-vitae.json'
-import ProfilPicture from '../images/angular.svg'
 import Typography from 'material-ui/Typography';
-import Card, { CardContent, CardMedia } from 'material-ui/Card';
+import Card, { CardContent } from 'material-ui/Card';
 import Icon from 'material-ui/Icon';
-import Divider from 'material-ui/Divider';
 
 const CvCard = (props) => {
 
     return (
         <Card className="crd-card">
-            <CardMedia
-                className="crd-cover"
-                image={ProfilPicture}
-                title="Profil picture"
-            />
             <CardContent className="crd-content">
-                <div className="crd-row">
-                    <Typography variant="headline">{resumeJSON.personalDetails.fullName}</Typography>
-                </div>
-                <Divider />
                 <div className="crd-row">
                     <Icon className="crd-leftIcon">phone</Icon>
                     <Typography variant="body1">{resumeJSON.personalDetails.tel}</Typography>
