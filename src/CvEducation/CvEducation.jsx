f2828b280cc55045ffe5f6e3f413ae62f4ef6290
don-sergio <saad.fatihi9@gmail.com>
2018-04-17 12:37:47 +0200

    create CvTimelineXs for small devices and update CvEducation cpt.

--- src/CvEducation/CvEducation.jsx
+++ src/CvEducation/CvEducation.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import './CvEducation.css'
+import PropTypes from 'prop-types';
+import compose from 'recompose/compose';
+import withWidth from 'material-ui/utils/withWidth';
 import resumeJSON from '../models/curriculum-vitae.json'
 import CvTimelineMd from '../CvTimeline/CvTimelineMd/CvTimelineMd';
+import CvTimelineXs from '../CvTimeline/CvTimelineXs/CvTimelineXs';
 import Typography from 'material-ui/Typography';
 import Card, { CardContent } from 'material-ui/Card';
 
 const CvEducationTitle = (props) => {
-    return <Typography align={props.align} variant="headline">{props.value}</Typography>
+    return <Typography align={props.align} variant="headline">{props.title}</Typography>
 }
 
 const CvEducationContent = (props) => {
     return (
         <Card className="edc-card">
+            {props.title && <Typography align='center' variant="headline">{props.title}</Typography>}
             <CardContent>
-                <Typography paragraph={true}>
-                    {props.value}
+                <Typography align={props.xs ? 'justify' : 'left'} paragraph={true}>
+                    {props.content}
                 </Typography>
             </CardContent>
         </Card>
@@ -23,20 +28,40 @@ const CvEducationContent = (props) => {
 
 const CvEducation = (props) => {
 
-    const listEducation = resumeJSON.education.list.map((item, index) => {
-        const align = index % 2 === 0 ? 'right' : 'left'
-        return {
-            id: item.id,
-            header: <CvEducationTitle align={align} value={item.period} />,
-            content: <CvEducationContent value={item.desc} />,
-            img: item.logo,
-        }
-    })
+    const { width } = props;
 
-    return (
-        <CvTimelineMd items={listEducation} />
-    )
+    if (width === 'xs' || width === 'sm') {
+
+        const listEducation = resumeJSON.education.list.map((item, index) => {
+            return {
+                id: item.id,
+                content: <CvEducationContent xs={true} title={item.period} content={item.desc} />,
+                img: item.logo,
+            }
+        })
+
+        return <CvTimelineXs items={listEducation} />
+
+    } else {
+
+        const listEducation = resumeJSON.education.list.map((item, index) => {
+            const align = index % 2 === 0 ? 'right' : 'left'
+            return {
+                id: item.id,
+                header: <CvEducationTitle align={align} title={item.period} />,
+                content: <CvEducationContent content={item.desc} />,
+                img: item.logo,
+            }
+        })
+
+        return <CvTimelineMd items={listEducation} />
+
+    }
+
 }
 
+CvEducation.propTypes = {
+    width: PropTypes.string.isRequired,
+};
 
-export default CvEducation;
+export default compose(withWidth())(CvEducation);
---
+++ src/CvTimeline/CvTimelineXs/CvTimelineXs.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './CvTimelineXs.css'
+
+const CvTimelineXs = (props) => {
+
+    const { items } = props
+
+    const tlxsRows = items.map((item, index) => {
+
+        const iconStyle = {
+            backgroundImage: `url(${require(`../../images/${item.img}`)})`
+        }
+
+        return (
+            <div key={item.id} className="tlxs-row">
+                <div className="tlxs-row-icon">
+                    <div className="tlxs-branch-vertical"></div>
+                    <div className="tlxs-icon" style={iconStyle}></div>
+                    <div className="tlxs-branch-vertical"></div>
+                </div>
+                <div className="tlxs-row-content">
+                    {item.content}
+                </div>
+            </div>
+        )
+    })
+
+    return (
+        <div className="tlxs-root">
+            {tlxsRows}
+        </div >
+    )
+}
+
+export default CvTimelineXs
