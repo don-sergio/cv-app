e51f300c110afb53ffc4fcf4b257a2b0de20be1e
don-sergio <saad.fatihi9@gmail.com>
2018-04-19 12:39:25 +0200

    update CvSkill cpt with chips.

--- src/CvSkills/CvSkill/CvSkill.jsx
+++ src/CvSkills/CvSkill/CvSkill.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
-// import './CvSkill.css'
-import Typography from 'material-ui/Typography'
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Avatar from 'material-ui/Avatar';
+import Chip from 'material-ui/Chip';
+// import img from '../../images/html.svg'
 
-const CvSkill = (props) => {
+function handleDelete() {
 
-    return <Typography>{props.children}</Typography>;
 }
 
-export default CvSkill;
+const styles = theme => ({
+    chip: {
+        margin: theme.spacing.unit / 2,
+    },
+    txtAvatar: {
+        backgroundColor: theme.palette.secondary.main,
+      }
+});
+
+const CvSkill = (props) => {
+    const { img, label, classes } = props;
+    let avatar = false
+    if (img === 'none') {
+        avatar = <Avatar className={classes.txtAvatar}>{label.charAt(0)}</Avatar>;
+    } else {
+        const imgUrl = require(`../../images/${img}`)
+        avatar = <Avatar src={imgUrl} />
+    }
+    return (
+        <Chip
+            avatar={avatar}
+            label={label}
+            onDelete={handleDelete}
+            className={classes.chip}
+        />
+    );
+}
+
+CvSkill.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CvSkill);
--- src/CvSkills/CvSkills.jsx
+++ src/CvSkills/CvSkills.jsx
@@ -1,42 +1,48 @@
 import React from 'react'
 import './CvSkills.css'
 import resumeJSON from '../models/curriculum-vitae.json'
+import PropTypes from 'prop-types';
+import withWidth from 'material-ui/utils/withWidth';
 import Typography from 'material-ui/Typography';
 import Card, { CardContent } from 'material-ui/Card';
 import CvSkill from './CvSkill/CvSkill';
 
 const CvSkills = (props) => {
+    
+    const { width } = props;
 
     const listWeb = resumeJSON.TechnicalSkills.webTechnologies.list.map((skill) =>
-        <CvSkill key={skill.id}>{skill.name}</CvSkill>
+        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
     );
     const listFrameworks = resumeJSON.TechnicalSkills.jsFrameworks.list.map((skill) =>
-        <CvSkill key={skill.id}>{skill.name}</CvSkill>
+        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
     );
     const listTools = resumeJSON.TechnicalSkills.tools.list.map((skill) =>
-        <CvSkill key={skill.id}>{skill.name}</CvSkill>
+        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
     );
     const listDatabases = resumeJSON.TechnicalSkills.databases.list.map((skill) =>
-        <CvSkill key={skill.id}>{skill.name}</CvSkill>
+        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
     );
+
+    const align = width === 'xs' ? 'center' : 'left'
 
     return (
         <Card>
             <CardContent className="skls-content">
                     <div className="skls-elem">
-                        <Typography gutterBottom variant="headline">{resumeJSON.TechnicalSkills.webTechnologies.title}</Typography>
+                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.webTechnologies.title}</Typography>
                         {listWeb}
                     </div>
                     <div className="skls-elem">
-                        <Typography gutterBottom variant="headline">{resumeJSON.TechnicalSkills.jsFrameworks.title}</Typography>
+                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.jsFrameworks.title}</Typography>
                         {listFrameworks}
                     </div>
                     <div className="skls-elem">
-                        <Typography gutterBottom variant="headline">{resumeJSON.TechnicalSkills.tools.title}</Typography>
+                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.tools.title}</Typography>
                         {listTools}
                     </div>
                     <div className="skls-elem">
-                        <Typography gutterBottom variant="headline">{resumeJSON.TechnicalSkills.databases.title}</Typography>
+                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.databases.title}</Typography>
                         {listDatabases}
                     </div>
             </CardContent>
@@ -44,4 +50,8 @@ const CvSkills = (props) => {
     )
 }
 
-export default CvSkills;
+CvSkills.propTypes = {
+    width: PropTypes.string.isRequired,
+};
+
+export default withWidth()(CvSkills);
