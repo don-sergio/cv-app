073944bb15c409fed46ca5847f882321fa60b6a9
don-sergio <saad.fatihi9@gmail.com>
2018-04-19 12:40:55 +0200

    display the screen width in CvToolbar.

--- src/CvToolbar/CvToolbar.jsx
+++ src/CvToolbar/CvToolbar.jsx
@@ -8,6 +8,8 @@ import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
 import Hidden from 'material-ui/Hidden';
+import compose from 'recompose/compose';
+import withWidth from 'material-ui/utils/withWidth';
 
 
 const styles = theme => ({
@@ -28,69 +30,71 @@ const styles = theme => ({
 
 
 const CvToolbar = (props) => {
-    const { classes } = props;
+    const { classes, width } = props;
     return (
-            <AppBar position="fixed">
-                <Toolbar>
-                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                        <Icon>home</Icon>
+        <AppBar position="fixed">
+            <Toolbar>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                    {/* <Icon>home</Icon> */}
+                    {width}
+                </IconButton>
+
+                <Hidden only={['xs', 'sm']}>
+                    <Button href="#skills" className={classes.button} color="inherit">
+                        {resumeJSON.TechnicalSkills.title}
+                        <Icon className={classes.rightIcon}>settings</Icon>
+                    </Button>
+                    <Button href="#education" className={classes.button} color="inherit">
+                        {resumeJSON.education.title}
+                        <Icon className={classes.rightIcon}>school</Icon>
+                    </Button>
+                    <Button href="#experience" className={classes.button} color="inherit">
+                        Expériences
+                        <Icon className={classes.rightIcon}>work</Icon>
+                    </Button>
+                    <Button href="#languages" className={classes.button} color="inherit">
+                        Langues
+                        <Icon className={classes.rightIcon}>language</Icon>
+                    </Button>
+
+                    <span className={classes.flex}></span>
+
+                    <Button className={classes.button} color="inherit">
+                        PDF
+                        <Icon className={classes.rightIcon}>file_download</Icon>
+                    </Button>
+                </Hidden>
+
+                <Hidden only={['md', 'lg', 'xl']}>
+
+                    <span className={classes.flex}></span>
+
+                    <IconButton href="#skills" color="inherit" aria-label="Menu">
+                        <Icon>settings</Icon>
+                    </IconButton>
+                    <IconButton href="#education" color="inherit" aria-label="Menu">
+                        <Icon>school</Icon>
+                    </IconButton>
+                    <IconButton href="#experience" color="inherit" aria-label="Menu">
+                        <Icon>work</Icon>
+                    </IconButton>
+                    <IconButton href="#languages" color="inherit" aria-label="Menu">
+                        <Icon>languages</Icon>
+                    </IconButton>
+                    <IconButton color="inherit" aria-label="Menu">
+                        <Icon>file_download</Icon>
                     </IconButton>
 
-                    <Hidden only={['xs', 'sm']}>
-                        <Button href="#skills" className={classes.button} color="inherit">
-                            {resumeJSON.TechnicalSkills.title}
-                            <Icon className={classes.rightIcon}>settings</Icon>
-                        </Button>
-                        <Button href="#education" className={classes.button} color="inherit">
-                            {resumeJSON.education.title}
-                            <Icon className={classes.rightIcon}>school</Icon>
-                        </Button>
-                        <Button href="#experience" className={classes.button} color="inherit">
-                            Expériences
-                        <Icon className={classes.rightIcon}>work</Icon>
-                        </Button>
-                        <Button href="#languages" className={classes.button} color="inherit">
-                            Langues
-                        <Icon className={classes.rightIcon}>language</Icon>
-                        </Button>
+                </Hidden>
 
-                        <span className={classes.flex}></span>
-
-                        <Button className={classes.button} color="inherit">
-                            PDF
-                        <Icon className={classes.rightIcon}>file_download</Icon>
-                        </Button>
-                    </Hidden>
-
-                    <Hidden only={['md', 'lg', 'xl']}>
-
-                        <span className={classes.flex}></span>
-
-                        <IconButton href="#skills" color="inherit" aria-label="Menu">
-                            <Icon>settings</Icon>
-                        </IconButton>
-                        <IconButton href="#education" color="inherit" aria-label="Menu">
-                            <Icon>school</Icon>
-                        </IconButton>
-                        <IconButton href="#experience" color="inherit" aria-label="Menu">
-                            <Icon>work</Icon>
-                        </IconButton>
-                        <IconButton href="#languages" color="inherit" aria-label="Menu">
-                            <Icon>languages</Icon>
-                        </IconButton>
-                        <IconButton color="inherit" aria-label="Menu">
-                            <Icon>file_download</Icon>
-                        </IconButton>
-
-                    </Hidden>
-
-                </Toolbar>
-            </AppBar>
+            </Toolbar>
+        </AppBar>
     );
 }
 
 CvToolbar.propTypes = {
     classes: PropTypes.object.isRequired,
+    width: PropTypes.string.isRequired
 };
 
-export default withStyles(styles)(CvToolbar);
+export default compose(withStyles(styles), withWidth())(CvToolbar);
