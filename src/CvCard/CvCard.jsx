0ccc557b2a4d94eb27461a45db91c6427b2d5ea4
don-sergio <saad.fatihi9@gmail.com>
2018-04-20 13:04:27 +0200

    change the color of icons.

--- src/CvCard/CvCard.jsx
+++ src/CvCard/CvCard.jsx
@@ -3,13 +3,20 @@ import './CvCard.css'
 import PropTypes from 'prop-types';
 import compose from 'recompose/compose';
 import withWidth from 'material-ui/utils/withWidth';
+import { withStyles } from 'material-ui/styles';
 import resumeJSON from '../models/curriculum-vitae.json'
 import Typography from 'material-ui/Typography';
 import Card, { CardContent } from 'material-ui/Card';
 import Icon from 'material-ui/Icon';
 
+const styles = theme => ({
+    leftIcon: {
+        color: theme.palette.primary.light,
+    }
+});
+
 const CvCard = (props) => {
-    const { width } = props;
+    const { width, classes } = props;
     const align = width === 'xs' || width === 'sm' ? 'center' : 'inherit'
     return (
         <Card>
@@ -23,15 +30,15 @@ const CvCard = (props) => {
                 <div className="crd-information">
                     <Typography gutterBottom align={align} variant="headline">{resumeJSON.personalDetails.title}</Typography>
                     <div className="crd-row">
-                        <Icon className="crd-leftIcon">phone</Icon>
+                        <Icon className={classes.leftIcon}>phone</Icon>
                         <Typography variant="body1">{resumeJSON.personalDetails.tel}</Typography>
                     </div>
                     <div className="crd-row">
-                        <Icon className="crd-leftIcon">email</Icon>
+                        <Icon className={classes.leftIcon}>email</Icon>
                         <Typography variant="body1">{resumeJSON.personalDetails.email}</Typography>
                     </div>
                     <div className="crd-row">
-                        <Icon className="crd-leftIcon">location_on</Icon>
+                        <Icon className={classes.leftIcon}>location_on</Icon>
                         <Typography variant="body1">{resumeJSON.personalDetails.address}</Typography>
                     </div>
                 </div>
@@ -42,6 +49,7 @@ const CvCard = (props) => {
 
 CvCard.propTypes = {
     width: PropTypes.string.isRequired,
+    classes: PropTypes.object.isRequired,
 };
 
-export default compose(withWidth())(CvCard);
+export default compose(withStyles(styles), withWidth())(CvCard);
