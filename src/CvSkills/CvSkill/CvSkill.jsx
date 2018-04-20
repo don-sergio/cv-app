6d87106532d7d573e8bee4538e3e06cc7df2a2f7
don-sergio <saad.fatihi9@gmail.com>
2018-04-20 12:48:41 +0200

    add handler and update closeIcons in chips cpts.

--- src/CvSkills/CvSkill/CvSkill.jsx
+++ src/CvSkills/CvSkill/CvSkill.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Avatar from 'material-ui/Avatar';
 import Chip from 'material-ui/Chip';
-// import img from '../../images/html.svg'
+import Icon from 'material-ui/Icon';
 
-function handleDelete() {
-
+function handleDelete(lien) {
+    window.open(lien, "_blank")
 }
 
 const styles = theme => ({
@@ -14,25 +14,33 @@ const styles = theme => ({
         margin: theme.spacing.unit / 2,
     },
     txtAvatar: {
-        backgroundColor: theme.palette.secondary.main,
-      }
+        backgroundColor: theme.palette.primary.light,
+        color: theme.palette.primary.contrastText
+    },
+    chipIcon: {
+        color: theme.palette.secondary.main,
+        '&:hover': {
+            color: theme.palette.secondary.dark,
+        },
+    }
 });
 
 const CvSkill = (props) => {
-    const { img, label, classes } = props;
+    const { skill, classes } = props;
     let avatar = false
-    if (img === 'none') {
-        avatar = <Avatar className={classes.txtAvatar}>{label.charAt(0)}</Avatar>;
+    if (skill.logo === 'none') {
+        avatar = <Avatar className={classes.txtAvatar}>{skill.name.charAt(0)}</Avatar>;
     } else {
-        const imgUrl = require(`../../images/${img}`)
+        const imgUrl = require(`../../images/${skill.logo}`)
         avatar = <Avatar src={imgUrl} />
     }
     return (
         <Chip
             avatar={avatar}
-            label={label}
-            onDelete={handleDelete}
+            label={skill.name}
+            onDelete={skill.certificated ? () => handleDelete(skill.link) : undefined}
             className={classes.chip}
+            deleteIcon={<Icon className={classes.chipIcon}>check_circle</Icon>}
         />
     );
 }
--- src/CvSkills/CvSkills.jsx
+++ src/CvSkills/CvSkills.jsx
@@ -8,20 +8,20 @@ import Card, { CardContent } from 'material-ui/Card';
 import CvSkill from './CvSkill/CvSkill';
 
 const CvSkills = (props) => {
-    
+
     const { width } = props;
 
     const listWeb = resumeJSON.TechnicalSkills.webTechnologies.list.map((skill) =>
-        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
+        <CvSkill key={skill.id} skill={skill} />
     );
     const listFrameworks = resumeJSON.TechnicalSkills.jsFrameworks.list.map((skill) =>
-        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
+        <CvSkill key={skill.id} skill={skill} />
     );
     const listTools = resumeJSON.TechnicalSkills.tools.list.map((skill) =>
-        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
+        <CvSkill key={skill.id} skill={skill} />
     );
     const listDatabases = resumeJSON.TechnicalSkills.databases.list.map((skill) =>
-        <CvSkill key={skill.id} img={skill.logo} label={skill.name} />
+        <CvSkill key={skill.id} skill={skill} />
     );
 
     const align = width === 'xs' ? 'center' : 'left'
@@ -29,22 +29,30 @@ const CvSkills = (props) => {
     return (
         <Card>
             <CardContent className="skls-content">
-                    <div className="skls-elem">
-                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.webTechnologies.title}</Typography>
+                <div className="skls-block">
+                    <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.webTechnologies.title}</Typography>
+                    <div className="skls-elems">
                         {listWeb}
                     </div>
-                    <div className="skls-elem">
-                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.jsFrameworks.title}</Typography>
+                </div>
+                <div className="skls-block">
+                    <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.jsFrameworks.title}</Typography>
+                    <div className="skls-elems">
                         {listFrameworks}
                     </div>
-                    <div className="skls-elem">
-                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.tools.title}</Typography>
+                </div>
+                <div className="skls-block">
+                    <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.tools.title}</Typography>
+                    <div className="skls-elems">
                         {listTools}
                     </div>
-                    <div className="skls-elem">
-                        <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.databases.title}</Typography>
+                </div>
+                <div className="skls-block">
+                    <Typography gutterBottom align={align} noWrap={true} variant="headline">{resumeJSON.TechnicalSkills.databases.title}</Typography>
+                    <div className="skls-elems">
                         {listDatabases}
                     </div>
+                </div>
             </CardContent>
         </Card>
     )
