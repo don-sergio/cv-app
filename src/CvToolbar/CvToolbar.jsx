ed13e68efec88fb7129fc4cf539e8d4b790fcf9f
don-sergio <saad.fatihi9@gmail.com>
2018-06-05 06:47:38 +0200

    update icones.

--- src/CvToolbar/CvToolbar.jsx
+++ src/CvToolbar/CvToolbar.jsx
@@ -7,10 +7,12 @@ import Toolbar from 'material-ui/Toolbar';
 import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import Icon from 'material-ui/Icon';
+import Avatar from 'material-ui/Avatar';
 import Hidden from 'material-ui/Hidden';
 import compose from 'recompose/compose';
 import withWidth from 'material-ui/utils/withWidth';
 import $ from 'jquery';
+import azgerLogo from '../images/azger-logo.svg';
 
 const styles = theme => ({
     flex: {
@@ -65,20 +67,7 @@ class CvToolbar extends React.Component {
             <AppBar position="fixed">
                 <Toolbar id="toolbar">
                     <IconButton className={classes.menuButton} color="inherit" aria-label="Menu" href="#">
-                    <svg height="56" width="56" version="1.2" baseProfile="tiny" id="Calque_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 453.5 453.5">
-                        <path fill="none" stroke="currentColor" strokeWidth="10" strokeMiterlimit="10" d="M297.8,233.8c0,0,22-10.5,43-12.5
-                            c24.5-3.5,33.5,3,33.5,3s-13,35-37,34.8S297.8,233.8,297.8,233.8z"/>
-                        <path fill="none" stroke="currentColor" strokeWidth="10" strokeMiterlimit="10" d="M189.8,301.7c-5.2,6.3-5.2,18.7-5.2,18.7
-                            c0,19,18.2,35.4,40.7,34.3c21.2-1,42.8-16.3,41.7-34c1.7-35.7-118.7-117.7-182-96.2c13.5,30.4,33,40,58,30.3
-                            c33.6,15,62.6,33.7,86.5,56.5c0,0,7.1,6.6,2.3,12.8c-5.2,6.8-16.8-1.8-16.8-1.8L189.8,301.7z"/>
-                        <path fill="none" stroke="currentColor" strokeWidth="10" strokeMiterlimit="10" d="M277.6,226.1c11.3-22,48.2-37.7,74.1-45.5
-                            s53.9-14,73.5-30.9c11.4-9.8,20.8-22.6,22-36.6c0.4-4.6-1.3-10.5-1.5-15.1c-3.8,24.1-28.5,41.9-54.5,50.1
-                            c-26.5,8.5-54.7,9.7-80.9,15.3c-26.8,5.7-54.3,17.5-66.8,39.2L277.6,226.1z"/>
-                        <path fill="none" stroke="currentColor" strokeWidth="10" strokeMiterlimit="10" d="M169.5,216.9c-19.2-12.9-45.5-29.7-67.7-36.3
-                            c-25.9-7.8-53.9-14-73.5-30.9c-11.4-9.8-20.8-22.6-22-36.6c-0.4-4.6,1.3-10.5,1.5-15.1c3.8,24.1,28.5,41.9,54.5,50.1
-                            c26.5,8.5,54.7,9.7,80.9,15.3c19.8,4.2,39.1,12.7,54.2,24.1c20.4,15.4,96,62.2,96,62.2s-21.5,35-30,29
-                            C243.3,264.4,191.1,231.4,169.5,216.9z"/>
-                    </svg>
+                    <Avatar src={azgerLogo} />
                     </IconButton>
 
                     <Hidden only={['xs', 'sm']}>
